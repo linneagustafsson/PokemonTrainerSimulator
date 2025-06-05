dddec148af0f026112f399a3e6675468591061c6
linne <linne@SALJE119>
2025-06-05 14:28:09 +0200

    added ponyta

---
+++ PokemonTrainerSimulator/Ponyta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTrainerSimulator
+{
+    internal class Ponyta : FirePokemon, IEvolvable
+    {
+        public Ponyta (int level, List<Attack> attacks) : base("Ponyta", level, attacks)
+        {
+        }
+        public void Evolve()
+        {
+            Name = "Rapidash";
+            Level += 10;
+            Console.WriteLine("Ponyta är på väg att utvecklas...");
+            Console.WriteLine($"Nu är det {Name}! Level {Level}!");
+        }
+    }
+}
--- PokemonTrainerSimulator/Program.cs
+++ PokemonTrainerSimulator/Program.cs
@@ -9,10 +9,13 @@ var bubble = new Attack("Bubble", ElementType.Water, 5);
 var thunderbolt = new Attack("Thunderbolt", ElementType.Electric, 15);
 var spark = new Attack("Spark", ElementType.Electric, 8);
 
+var flashfire = new Attack("Flash Fire", ElementType.Fire, 20);
+
 //skapar pokemon
 var charmander = new Charmander(5, new List<Attack> { flamethrower, ember });
 var squirtle = new Squirtle(7, new List<Attack> { waterGun, bubble });
 var pikachu = new Pikachu(10, new List<Attack> { thunderbolt, spark });
+var ponyta = new Ponyta(4, new List<Attack> { flashfire, ember });
 
 
 List<Pokemon> trainerPokemons = new List<Pokemon>{ charmander, squirtle, pikachu };
@@ -21,12 +24,23 @@ foreach (var pokemon in trainerPokemons)
 {
     pokemon.RaiseLevel();
     pokemon.RandomAttack();
+   pokemon.Attack();
 
-    if(pokemon is IEvolvable evolvablePokemon) 
+    if (ponyta is IEvolvable evolvablePonyta)
     {
-        evolvablePokemon.Evolve();
+        ponyta.Evolve();
     }
+
+    //if (pokemon is IEvolvable evolvablePokemon) 
+    //{
+    //    evolvablePokemon.Evolve();
+    //}
+
+    //Console.WriteLine($"Pokemon: {pokemon.Name}, Level: {pokemon.Level}");
+
     Console.ReadKey();
 }
 
-    
+
+
+
